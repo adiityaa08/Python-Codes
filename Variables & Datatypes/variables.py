54f93bc62275062c6fb9cec3406095438fd3a5b1
adiityaa08 <adityasathawane2003@gmail.com>
2025-01-13 01:32:54 +0530

    Update variables.py

--- Variables & Datatypes/variables.py
+++ Variables & Datatypes/variables.py
@@ -1,1 +1,27 @@
+name="aditya" #string
+print("my name is:",name)
 
+age=22 # int
+print("the age is:",age)
+
+price=25.99 # float
+print(price)
+
+# type()-return the datatype of varialbe
+print(type(name))
+print(type(age))
+print(type(price))
+
+a=None
+b=True
+print(type(a))
+print(type(b))
+
+a=2
+b=2
+print(a+b)
+
+# comments
+'''
+hi aditya
+'''
